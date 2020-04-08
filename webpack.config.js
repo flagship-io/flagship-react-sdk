1451aebe5307ab5973848a324265a07f0006911c
Emilien Domenge <emilien.domenge-heritier@abtasty.com>
2020-04-08 14:44:44 +0200

    kill babel & webpack

--- webpack.config.js
+++
@@ -1,1 +0,0 @@
-module.exports = (env) => require(`./webpack/config.${env}.js`);
--- webpack/config.stats.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const devConfig = require('./config.dev.js');
-
-module.exports = merge(devConfig, {
-  plugins: [
-    new BundleAnalyzerPlugin(),
-  ],
-});
