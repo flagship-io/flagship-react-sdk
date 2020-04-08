7c98fe7d87722a8b3379df4af60350fe60579aeb
Emilien Domenge <emilien.domenge-heritier@abtasty.com>
2020-04-08 09:58:13 +0200

    tsc build

--- src/withFlagship.tsx
+++ src/withFlagship.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import flagship from '@flagship.io/js-sdk';
+import React, { useState, useEffect, ReactNode } from "react";
+import flagship from "@flagship.io/js-sdk";
 
-export default function withFlagship(WrappedComponent, reactSdkConfig, envId, visitorInfo = { id: 'test-perf', context: {} }): React.ReactNode {
-  const [fsVisitor, setFsVisitor] = React.useState(null);
-  React.useEffect(() => {
+export default function withFlagship(
+  WrappedComponent,
+  reactSdkConfig,
+  envId,
+  visitorInfo = { id: "test-perf", context: {} }
+): ReactNode {
+  const [fsVisitor, setFsVisitor] = useState(null);
+  useEffect(() => {
     const { id, context } = visitorInfo;
 
     const fsSdk = flagship.initSdk(envId, { ...reactSdkConfig });
 
     const visitorInstance = fsSdk.newVisitor(id, context);
 
-    visitorInstance.on('ready', () => {
+    visitorInstance.on("ready", () => {
       setFsVisitor(visitorInstance);
     });
   }, [envId, reactSdkConfig, visitorInfo]);
-  const reactComponent = (props): React.ReactNode => <WrappedComponent fsVisitor={fsVisitor} {...props} />;
+  const reactComponent = (props): ReactNode => (
+    <WrappedComponent fsVisitor={fsVisitor} {...props} />
+  );
   return reactComponent;
 }
