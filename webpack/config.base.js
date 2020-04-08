5c2f25fc2b470c8842a2271ce4468076e8d299e6
Emilien Domenge <emilien.domenge-heritier@abtasty.com>
2020-04-08 12:00:00 +0200

    Update

--- webpack/config.base.js
+++ webpack/config.base.js
@@ -1,23 +1,22 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
   entry: {
-    main: './src/index.tsx',
-    FsContext: './src/FlagshipContext.tsx',
-    'FsContext.min': './src/FlagshipContext.tsx',
-    FsHooks: './src/FlagshipHooks.tsx',
-    'FsHooks.min': './src/FlagshipHooks.tsx',
-
+    index: "./src/index.tsx",
+    FsContext: "./src/FlagshipContext.tsx",
+    "FsContext.min": "./src/FlagshipContext.tsx",
+    FsHooks: "./src/FlagshipHooks.tsx",
+    "FsHooks.min": "./src/FlagshipHooks.tsx"
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   output: {
     // filename: 'index.js',
-    filename: '[name].js',
-    path: path.resolve(__dirname, '../dist'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "../dist"),
     // library: 'flagship',
-    libraryTarget: 'commonjs2',
+    libraryTarget: "commonjs2"
     // libraryExport: 'default',
   },
   // target: 'node', // TODO:
@@ -30,11 +29,11 @@ module.exports = {
         exclude: /(node_modules|bower_components|dist)/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
+              presets: ["@babel/preset-env"]
+            }
+          }
           // {
           //   loader: 'awesome-typescript-loader',
           // },
@@ -44,41 +43,41 @@ module.exports = {
           //     // eslint options (if necessary)
           //   },
           // },
-        ],
+        ]
       },
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-            },
+              presets: ["@babel/preset-env"]
+            }
           },
           {
-            loader: 'eslint-loader',
+            loader: "eslint-loader",
             options: {
               // eslint options (if necessary)
-            },
-          },
-        ],
-      },
-    ],
+            }
+          }
+        ]
+      }
+    ]
   },
   externals: [
     {
-      react: 'commonjs react',
+      react: "commonjs react"
       // reactDOM: 'react-dom',
-    },
+    }
   ],
-  plugins: [new webpack.DefinePlugin({ 'global.GENTLY': false })],
+  plugins: [new webpack.DefinePlugin({ "global.GENTLY": false })],
   resolve: {
     alias: {
-      react: path.resolve('./node_modules/react'),
+      react: path.resolve("./node_modules/react")
 
       // react: require.resolve('react'),
     },
-    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
-  },
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  }
 };
