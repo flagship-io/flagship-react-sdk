bba2eb4450057fc435fa683efd69806ee35f54f2
Emilien Domenge <emilien.domenge-heritier@abtasty.com>
2020-04-15 16:07:05 +0200

    Activate demo

--- examples/react-dev-demo/src/components/AppContainer/components/DemoUseFsActivate/index.tsx
+++ examples/react-dev-demo/src/components/AppContainer/components/DemoUseFsActivate/index.tsx
@@ -1,37 +1,38 @@
-import { useFsActivate } from "@flagship.io/react-sdk";
-import CodeBlock from "@tenon-io/tenon-codeblock";
-import React, { useEffect, useState } from "react";
-import { Alert, Col, Row } from "react-bootstrap";
+import { useFsActivate } from '@flagship.io/react-sdk';
+import CodeBlock from '@tenon-io/tenon-codeblock';
+import React, { useEffect, useState } from 'react';
+import { Alert, Col, Row } from 'react-bootstrap';
 
 export const DemoUseFsActivate = () => {
-  const fsModifications = useFsActivate([
-    {
-      key: "btnColor",
-      defaultValue: "green",
-      activate: false,
-    },
-  ]);
-  const demoHookName = "useFsActivate";
-  return (
-    <Row>
-      <Col>
-        <a className="fsAnchor" id={demoHookName} />
-        <Alert variant="dark" className="fs-alert demoHook">
-          <Alert.Heading>{demoHookName}</Alert.Heading>
-          <p>
-            Use <b>{demoHookName}</b> hook to get the modifications:
-          </p>
-          <CodeBlock
-            className="mv3"
-            codeString={`const fsModifications = useFsModificationsCache([{ key: "btn-color", defaultValue: "GREEN", activate: false }]);`}
-          />
-          <p>Output: </p>
-          <CodeBlock
-            className="mv3"
-            codeString={JSON.stringify(fsModifications)}
-          />
-        </Alert>
-      </Col>
-    </Row>
-  );
+    debugger;
+    useFsActivate(['btnColor', 'otherKey1', 'otherKey2']);
+    const demoHookName = 'useFsActivate';
+    return (
+        <Row>
+            <Col>
+                <a className="fsAnchor" id={demoHookName} />
+                <Alert variant="dark" className="fs-alert demoHook">
+                    <Alert.Heading>{demoHookName}</Alert.Heading>
+                    <p>
+                        Use <b>{demoHookName}</b> hook to trigger activation of
+                        a modification:
+                    </p>
+                    <CodeBlock
+                        className="mv3"
+                        codeString={`useFsActivate([
+    'btnColor',
+    'otherKey1',
+    'otherKey2'
+]);                        `}
+                    />
+                    <div>
+                        In this example, keys{' '}
+                        <i>btnColor, otherKey1, otherKey2</i> have been
+                        activated. You can check on networks, a http request
+                        "activate" for each key specified in the array.
+                    </div>
+                </Alert>
+            </Col>
+        </Row>
+    );
 };
--- examples/react-dev-demo/src/components/AppContainer/components/DemoUseFsModificationsCache/index.tsx
+++ examples/react-dev-demo/src/components/AppContainer/components/DemoUseFsModificationsCache/index.tsx
@@ -1,37 +1,57 @@
-import { useFsModificationsCache } from "@flagship.io/react-sdk";
-import CodeBlock from "@tenon-io/tenon-codeblock";
-import React, { useEffect, useState } from "react";
-import { Alert, Col, Row } from "react-bootstrap";
+import { useFsModificationsCache } from '@flagship.io/react-sdk';
+import CodeBlock from '@tenon-io/tenon-codeblock';
+import React, { useEffect, useState } from 'react';
+import { Alert, Col, Row } from 'react-bootstrap';
 
 export const DemoUseFsModificationsCache = () => {
-  const fsModifications = useFsModificationsCache([
-    {
-      key: "btnColor",
-      defaultValue: "green",
-      activate: false,
-    },
-  ]);
-  const demoHookName = "useFsModificationsCache";
-  return (
-    <Row>
-      <Col>
-        <a className="fsAnchor" id={demoHookName} />
-        <Alert variant="dark" className="fs-alert demoHook">
-          <Alert.Heading>{demoHookName}</Alert.Heading>
-          <p>
-            Use <b>{demoHookName}</b> hook to get the modifications:
-          </p>
-          <CodeBlock
-            className="mv3"
-            codeString={`const fsModifications = useFsModificationsCache([{ key: "btn-color", defaultValue: "GREEN", activate: false }]);`}
-          />
-          <p>Output: </p>
-          <CodeBlock
-            className="mv3"
-            codeString={JSON.stringify(fsModifications)}
-          />
-        </Alert>
-      </Col>
-    </Row>
-  );
+    const fsModifications = useFsModificationsCache([
+        {
+            key: 'btnColor',
+            defaultValue: 'green',
+            activate: false
+        }
+    ]);
+    const demoHookName = 'useFsModificationsCache';
+    return (
+        <Row>
+            <Col>
+                <a className="fsAnchor" id={demoHookName} />
+                <Alert variant="dark" className="fs-alert demoHook">
+                    <Alert.Heading>{demoHookName}</Alert.Heading>
+                    <p>
+                        Use <b>{demoHookName}</b> hook to get the modifications:
+                    </p>
+                    <CodeBlock
+                        className="mv3"
+                        codeString={`import { useFsModificationsCache } from '@flagship.io/react-sdk';
+const fsModifications = useFsModificationsCache([
+  {
+      key: 'btnColor',
+      defaultValue: 'green',
+      activate: false
+  }
+]);              `}
+                    />
+                    <p>Output: </p>
+                    <div>
+                        <button
+                            style={{
+                                backgroundColor: fsModifications.btnColor
+                            }}
+                        >
+                            I'm a button customized with Flagship
+                        </button>
+                    </div>
+                    <CodeBlock
+                        className="mv3"
+                        codeString={`<div>
+    <button style={{ backgroundColor: ${fsModifications.btnColor} }}>
+        I'm a button customized with Flagship
+    </button>
+</div>              `}
+                    />
+                </Alert>
+            </Col>
+        </Row>
+    );
 };
