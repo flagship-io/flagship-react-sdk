c071811509f3eda5a8e68a6d5077738f7aeef523
Emilien Domenge <emilien.domenge-heritier@abtasty.com>
2020-04-30 16:57:05 +0200

    WIP QA - useActivate

--- examples/react-dev-demo/src/components/AppContainer/components/DemoUseFsActivate/index.tsx
+++ examples/react-dev-demo/src/components/AppContainer/components/DemoUseFsActivate/index.tsx
@@ -1,62 +1,23 @@
-import { useFsActivate } from '@flagship.io/react-sdk';
-import CodeBlock from '../../../common/CodeBlock';
-import React from 'react';
-import { Alert, Col, Row, Button } from 'react-bootstrap';
+import React, { useContext } from 'react';
+import { Alert, Col, Row } from 'react-bootstrap';
+import PlayWithActivate from './components/normal';
+import { AppSettings, SettingContext } from '../../../../App';
+import PlayWithActivateQA from './components/qa';
 
-export const DemoUseFsActivate = () => {
-    const [toggle, setToggle] = React.useState(false);
-    useFsActivate(['btnColor', 'otherKey1', 'otherKey2'], [toggle]);
+export const DemoUseFsActivate: React.FC = () => {
     const demoHookName = 'useFsActivate';
+    const { QA } = useContext(SettingContext) as AppSettings;
     return (
         <Row>
             <Col>
                 <div className="fsAnchor" id={demoHookName} />
                 <Alert variant="dark" className="fs-alert demoHook">
                     <Alert.Heading>{demoHookName}</Alert.Heading>
-                    <p>
-                        Use <b>{demoHookName}</b> hook to trigger activation of
-                        a modification when needed:
-                    </p>
-                    <CodeBlock
-                        className="mv3"
-                        codeString={`const [toggle, setToggle] = React.useState(false);
-useFsActivate(['btnColor', 'otherKey1', 'otherKey2'], [toggle]);
-
-// {...}
-
-<Button
-variant="secondary"
-onClick={() => setToggle(!toggle)}
->
-    Trigger activate
-</Button>`}
-                    />
-                    <Button
-                        variant="secondary"
-                        onClick={() => setToggle(!toggle)}
-                    >
-                        Trigger activate
-                    </Button>
-                    <div className="mv3">
-                        <p>
-                            In this example, we're activating only when{' '}
-                            <i>toggle</i> value has changed as we have specified
-                            a 2nd argument in <i>useFsActivate</i> hook.
-                        </p>
-                        <p>
-                            We also specified in 1st argument that we want to
-                            activate 3 keys. For each key, there is 2 possible
-                            scenarios:
-                        </p>
-                        <p>
-                            If the key exist, a http request "activate" will be
-                            done.
-                        </p>
-                        <p>
-                            If the key does not exist, you will receive a
-                            warning log.
-                        </p>
-                    </div>
+                    {QA.enabled ? (
+                        <PlayWithActivateQA></PlayWithActivateQA>
+                    ) : (
+                        <PlayWithActivate></PlayWithActivate>
+                    )}
                 </Alert>
             </Col>
         </Row>
