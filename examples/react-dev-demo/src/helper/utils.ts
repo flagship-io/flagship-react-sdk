bc82b5f3454a2737474dbd5b6e222d630d679675
Madorakkusu <yanistam@hotmail.fr>
2021-06-02 11:18:27 +0200

    Try catch on localStorage

--- examples/react-dev-demo/src/helper/utils.ts
+++ examples/react-dev-demo/src/helper/utils.ts
@@ -1,28 +1,33 @@
 const LOCAL_STORAGE_NODE = 'fsDevDemo';
 
 export const saveLocalStorage: (callback: (oldPayload: any) => any) => { success: boolean; message: string } = (
-    callback
+  callback,
 ) => {
-    const oldPayload = getLocalStorage();
-    const newPayload = callback(oldPayload);
-    if (newPayload && newPayload.constructor !== Object) {
-        return {
-            success: false,
-            message: 'the new payload is not an object'
-        };
-    }
-    if (newPayload === null) {
-        localStorage.removeItem(LOCAL_STORAGE_NODE);
-    } else {
-        localStorage.setItem(LOCAL_STORAGE_NODE, JSON.stringify(newPayload));
-    }
+  const oldPayload = getLocalStorage();
+  const newPayload = callback(oldPayload);
+  if (newPayload && newPayload.constructor !== Object) {
     return {
-        success: true,
-        message: 'ok'
+      success: false,
+      message: 'the new payload is not an object',
     };
+  }
+  if (newPayload === null) {
+    localStorage.removeItem(LOCAL_STORAGE_NODE);
+  } else {
+    localStorage.setItem(LOCAL_STORAGE_NODE, JSON.stringify(newPayload));
+  }
+  return {
+    success: true,
+    message: 'ok',
+  };
 };
 
 export const getLocalStorage = () => {
-    const str = localStorage.getItem(LOCAL_STORAGE_NODE);
-    return !str ? null : JSON.parse(str);
+  let str;
+  try {
+    str = localStorage.getItem(LOCAL_STORAGE_NODE);
+  } catch {
+    str = null;
+  }
+  return !str ? null : JSON.parse(str);
 };
