c13383ed71db99c7d23f324977ded8b9d933abee
Merveille KITOKO <merkitoko@gmail.com>
2021-11-04 13:08:50 +0100

    demo

--- example/test-react-dev/src/App.tsx
+++ example/test-react-dev/src/App.tsx
@@ -38,15 +38,17 @@ const loadingComponent = ()=>{
     Lorem ipsum dolor sit amet consectetur, adipisicing elit. At doloremque neque eveniet voluptatem dicta optio sint quam vero tempore! Tempora exercitationem recusandae numquam dicta illum quisquam non est distinctio ad!
   </div>
 }
+
+let countRenderApp = 0
 function App() {
   const [visitorData,setVisitorData] = useState<IVisitorData>(initStat.visitorData)
-  const [dynamicProp, setDynamicProp] = useState(1);
+  const [dynamicProp, setDynamicProp] = useState(0);
   useEffect(()=>{
-    let count = 1
+    let count = 0
     const intervalID = setInterval(()=>{
-      count++
-      setDynamicProp(Math.random());
-    }, 2000)
+      count+=5
+      setDynamicProp(count);
+    }, 5000)
     return ()=>{
       clearInterval(intervalID)
     }
@@ -61,9 +63,15 @@ function App() {
     console.log("count",count);
     setVisitorData({...visitorData,id:'visitor_'+ count, isAuthenticated: !visitorData.isAuthenticated})
   }
+
+  setTimeout(() => {
+    console.log("render app", countRenderApp++);
+  }, 1000);
+  
+  
   return(
-  <FlagshipProvider decisionApiUrl={"https://decision.flagship.io/v2/"}  visitorData={visitorData} pollingInterval={5}  envId={ENV_ID} timeout={5} apiKey={API_KEY} fetchNow={true} >
-    <Home dynamicProp={0} />
+  <FlagshipProvider decisionApiUrl={"https://decision.flagship.io/v2/"} activateDeduplicationTime={20}   visitorData={visitorData} pollingInterval={5}  envId={ENV_ID} timeout={5} apiKey={API_KEY} fetchNow={true} >
+    <Home dynamicProp={dynamicProp} />
     <button style={{width:100, height:50}} onClick={()=>{onClick()}}>click me</button>
   </FlagshipProvider>
   )
