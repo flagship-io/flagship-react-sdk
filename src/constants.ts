9e9c2b2b316c4f2b1dc20a976eddcf1e49c396ce
Merveille KITOKO <merkitoko@gmail.com>
2022-01-26 04:13:11 +0100

    add constants

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const noVisitorMessage =
+  'sdk not correctly initialized... Make sure fsVisitor is ready.'
+export const noVisitorDefault =
+  'fsVisitor not initialized, returns default value'
