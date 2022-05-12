a7beff463941b115245737b58aff4082326ff891
Heihokon <neoshark22@gmail.com>
2022-05-12 11:30:52 +0100

    add new Constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const noVisitorMessage = 'flagship Visitor not initialized.'
-export const noVisitorDefault =
-  'fsVisitor not initialized, returns default value'
+export const noVisitorDefault = 'fsVisitor not initialized, returns default value'
+
+  export const GET_FLAG_CAST_ERROR ='Flag for key {0} has a different type. Default value is returned.'
+  export const GET_METADATA_CAST_ERROR = 'Flag for key {0} has a different type with defaultValue, an empty metadata object is returned'
