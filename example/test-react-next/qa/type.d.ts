cc66887f58b6ccc5b959c678854691f0398b4067
Heihokon <neoshark22@gmail.com>
2023-08-02 16:05:43 -0500

    add type

---
+++ example/test-react-next/qa/type.d.ts
@@ -0,0 +1,44 @@
+
+
+export type Campaign = {
+    id: string
+    type: string
+    slug?:string|null
+    variationGroups:VariationGroup[]
+}
+
+export type Targetings = {
+    operator: string;
+    key: string;
+    value: any;
+}
+
+export type Variation = {
+    id: string;
+    modifications: {
+        type: string;
+        value: any;
+    };
+    allocation?: number;
+    reference?: boolean;
+    isSelected?: boolean
+}
+export type VariationGroup = {
+    id: string;
+    targeting: {
+        targetingGroups: Array<{
+            targetings: Array<Targetings>;
+        }>;
+    };
+    variations: Variation[];
+}
+export type BucketingDTO = {
+    panic?: boolean;
+    campaigns?: Campaign[];
+}
+
+export type ExposedVariations ={
+    campaignId: string,
+    variationGroupId: string,
+    variationId: string
+  }
