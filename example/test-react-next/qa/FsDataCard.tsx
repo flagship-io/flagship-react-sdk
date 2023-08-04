49469000cb8081eb0e4ab8a9b7ddabe05422392f
Heihokon <neoshark22@gmail.com>
2023-08-04 15:25:50 -0500

    working [search ]

--- example/test-react-next/qa/FsDataCard.tsx
+++ example/test-react-next/qa/FsDataCard.tsx
@@ -8,6 +8,7 @@ import style from "./index.module.css";
 import { BucketingDTO, Campaign, ExposedVariations, ForcedVariation } from "./type";
 import { CampaignItem } from "./CampaignItem";
 import { fsModuleQAContext } from "./fsModuleQAContext";
+import { Header } from "./Header";
 
 export type FsDataCardProps = {
     onVariationsForced: (forcedVariations: ForcedVariation[]) => void
@@ -19,9 +20,11 @@ export type FsDataCardProps = {
 export function FsDataCardFunc(props: FsDataCardProps) {
     const { onVariationsForced, bucketing, onArrowClick } = props
     const [campaigns, setCampaigns] = useState<Campaign[]>([]);
+    const [computeCampaigns, setComputeCampaigns] = useState(false)
     const exposedVariations = props.exposedVariations();
     const forceVariationsRef = useRef<ForcedVariation[]>([])
-
+    const campaignsRef = useRef<Campaign[]>([])
+  
     console.log("FsDataCard");
 
     useEffect(() => {
@@ -51,7 +54,8 @@ export function FsDataCardFunc(props: FsDataCardProps) {
         });
 
         setCampaigns(localCampaigns);
-    }, [JSON.stringify(exposedVariations), bucketing?.campaigns]);
+        campaignsRef.current = localCampaigns
+    }, [JSON.stringify(exposedVariations), bucketing?.campaigns, computeCampaigns]);
 
     const onVariationSelected = useCallback((item: ExposedVariations) => {
         const forceVariations = forceVariationsRef.current
@@ -67,18 +71,45 @@ export function FsDataCardFunc(props: FsDataCardProps) {
         onVariationsForced(forceVariationsRef.current)
     }
 
+    const onSearchInputChange = useCallback((value:string)=>{
+        if (!value) {
+            setComputeCampaigns(state=> !state)
+            return 
+        }
+        const localCampaigns: Campaign[] = [];
+        campaignsRef.current.forEach(campaign=>{
+            if (campaign.id.includes(value)) {
+                localCampaigns.push(campaign)
+                return 
+            }
+            campaign.variationGroups.forEach(variationGroup=>{
+                if (variationGroup.id.includes(value)) {
+                    localCampaigns.push(campaign)
+                    return 
+                }
+                variationGroup.variations.forEach(variation=>{
+                    if (variation.id.includes(value)) {
+                        localCampaigns.push(campaign)
+                        return
+                    }
+                    if (typeof variation.modifications.value === 'object') {
+                        const flags = variation.modifications.value
+                        for (const key in flags) {
+                            if (key.includes(value) || `${flags[key]}`.includes(value)) {
+                                localCampaigns.push(campaign)
+                                return
+                            }
+                        }
+                    }
+                })
+            })
+        });
+        setCampaigns(localCampaigns)
+    },[])
+
     return (
         <div className={style.card}>
-            <div className={`${style["card-header"]} ${style["card-header-d-flex"]}`}>
-                <div>Header</div>
-                <div>
-                    <div onClick={onValidation} >Yes</div>
-                    <div className={style["close-card"]} onClick={onArrowClick}>
-                        X
-                    </div>
-                </div>
-
-            </div>
+            <Header onArrowClick={onArrowClick} onValidation={onValidation} onSearchInputChange={onSearchInputChange} />
             <div className={style["card-body"]}>
                 <fsModuleQAContext.Provider value={{ onVariationSelected }}>
                     {campaigns.map((item, index) => {
