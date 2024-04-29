0af58dce3b579c04b5153974b7d09bbafe68f52b
Heihokon <neoshark22@gmail.com>
2024-04-29 12:16:53 -0500

    Update package versions and remove unnecessary files

--- example/test-react-dev/src/constants/errorMessage.ts
+++
@@ -1,2 +0,0 @@
-export const ERROR_SDK_NOT_READY = "First, set your Flagship Environment ID & API Key"
-export const ERROR_VISITOR_NOT_SET = 'Set your Visitor ID and context'
--- example/test-react-next-13/src/components/MyFlagComponent.js
+++
@@ -1,9 +0,0 @@
-'use client'
-import {
-  useFsFlag
-} from '@flagship.io/react-sdk'
-
-export function MyFlagComponent () {
-  const myFlag = useFsFlag('my_flag_key', 'default-value')
-  return <p>flag value: {myFlag.getValue()}</p>
-}
