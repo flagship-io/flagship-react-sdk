299b9a07f8d6d4e18992099419d3dee906d8ef2d
Heihokon <neoshark22@gmail.com>
2024-04-29 16:36:05 -0500

    Add itemData.ts file with initial item data

---
+++ demo/src/itemData.ts
@@ -0,0 +1,11 @@
+
+export const itemData = {
+    title: "Duvet cover",
+    subtitle: "Eloge floral",
+    refNumber: "Ref: 123456",
+    price: "€99.99",
+    imageUrl: "https://source.unsplash.com/white-and-grey-bedroom-interior-Cx188P-L_HY",
+    imageAlt: "Contemplative Reptile",
+    imageTitle: "Contemplative Reptile",
+    discountPrice: "€79.99",
+}
