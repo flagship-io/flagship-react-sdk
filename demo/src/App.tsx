f698eab4a6512abfe148ef056a1e2b5b643b3cf7
Heihokon <neoshark22@gmail.com>
2024-04-29 16:36:17 -0500

    Update Flagship integration in App component

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -2,22 +2,40 @@
 import "./App.css";
 import FlagshipProvider from "@flagship.io/react-sdk";
 import { Item } from "./Item";
+import { Container, FormControlLabel, Grid, Switch } from "@mui/material";
+import { useState } from "react";
 
 function App() {
+  const [isVip, setIsVip] = useState(false);
   return (
-    <FlagshipProvider
-      envId="<ENV_ID>"
-      apiKey="<API_KEY>"
-      visitorData={{
-        id: "test-user-id", 
-        hasConsented: true,
-        context: {
-          fs_is_vip: true,
-        },
-      }}
-    >
-      <Item />
-    </FlagshipProvider>
+    <Container maxWidth="xs">
+      <Grid item xs={12}>
+        <FormControlLabel
+          control={
+            <Switch
+              defaultChecked
+              checked={isVip}
+              onChange={() => setIsVip(!isVip)}
+            />
+          }
+          label="Is vip client"
+        />
+      </Grid>
+      {/* Step 1: Initialize the SDK with FlagshipProvider */}
+      <FlagshipProvider
+        envId="<ENV_ID>"
+        apiKey="<API_KEY>"
+        visitorData={{
+          id: "visitor_id",
+          hasConsented: true,
+          context: {
+            fs_is_vip: isVip,
+          },
+        }}
+      >
+        <Item />
+      </FlagshipProvider>
+    </Container>
   );
 }
 
