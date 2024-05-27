699b8d4df4c28d7e2817c72b476d94f31cc57179
Heihokon <neoshark22@gmail.com>
2024-05-27 14:17:04 -0500

    refactor: Update import order in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,31 +23,31 @@ module.exports = {
     '@typescript-eslint'
   ],
   rules: {
-    "no-use-before-define": ["error", { "variables": false }],
-    "import/order": [
+    'no-use-before-define': ['error', { variables: false }],
+    'import/order': [
       2,
       {
-        "groups": ["builtin", "external", "internal", ["index", "sibling", "parent"]],
-        "pathGroups": [
+        groups: ['builtin', 'external', 'internal', ['index', 'sibling', 'parent']],
+        pathGroups: [
           {
             // Configure react libs to be on top of external modules
-            "pattern": "{react*,react*/**}",
-            "group": "external",
-            "position": "before"
+            pattern: '{react*,react*/**}',
+            group: 'external',
+            position: 'before'
           },
           {
             // Configure @flagship.io to be last external modules
-            "pattern": "@flagship.io/**",
-            "group": "internal",
-            "position": "after"
-          },
+            pattern: '@flagship.io/**',
+            group: 'internal',
+            position: 'after'
+          }
         ],
-        "alphabetize": {
-          "order": "asc",
-          "caseInsensitive": true
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
         },
-        "newlines-between": "always",
-        "pathGroupsExcludedImportTypes": ["builtin"]
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['builtin']
       }
     ]
   },
