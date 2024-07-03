37a78e7a575fe9a8e71d9509a63e2885c21140e9
Heihokon <neoshark22@gmail.com>
2024-07-03 09:27:12 -0500

    chore: Update item image URL to use local file

--- demo/src/itemData.ts
+++ demo/src/itemData.ts
@@ -4,7 +4,7 @@ export const itemData = {
     subtitle: "Eloge floral",
     refNumber: "Ref: 123456",
     price: "€99.99",
-    imageUrl: "https://source.unsplash.com/white-and-grey-bedroom-interior-Cx188P-L_HY",
+    imageUrl: "./duvet_cover.jpg",
     imageAlt: "Contemplative Reptile",
     imageTitle: "Contemplative Reptile",
     discountPrice: "€79.99",
